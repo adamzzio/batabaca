905e00a76ef379498d59cdebb2307bd69e0c9599
Muhammad Hanif Sudibyo <mhsudibyo@gmail.com>
2022-11-29 13:38:58 +0700

    Change foto profil

--- 00_🎈_About_Us.py
+++ 00_🎈_About_Us.py
@@ -23,10 +23,10 @@ st.write('## Anggota Tim :')
 col1, col2 = st.columns(2)
 col3, col4 = st.columns(2)
 
-foto_adam = Image.open('foto_adam4.png').resize((400,400))
+foto_adam = Image.open('foto_adam.jpg').resize((400,400))
 foto_alya = Image.open('Bebeb batik.jpg').resize((400,400))
 foto_hanif = Image.open('foto_hanif.jpg').resize((400,400))
-foto_naura = Image.open('foto_annaura3.png').resize((400,400))
+foto_naura = Image.open('foto_naura.png').resize((400,400))
 
 # For columns 1 : Introduce Adam Maurizio Winata
 col1.write('### Adam Maurizio Winata')
